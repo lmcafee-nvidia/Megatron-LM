769605b2336d802f8c011f62564ebece023bda57
Lawrence McAfee <lmcafee@nvidia.com>
2026-09-03 19:24:07 -0400

    Skip prefill score when stop trim removes every token
    Signed-off-by: Lawrence McAfee <lmcafee@nvidia.com>

--- megatron/core/inference/engines/dynamic_engine.py
+++ megatron/core/inference/engines/dynamic_engine.py
@@ -1520,7 +1520,7 @@ class DynamicInferenceEngine(AbstractEngine):
                     # We only want decode log probs.
                     if is_chunked_prefill:
                         pass
-                    elif is_prefill and len(request_log_probs) > 0:
+                    elif is_prefill and request.generated_tokens and len(request_log_probs) > 0:
                         request.generated_log_probs.append(request_log_probs[-1])
                     else:
                         request.generated_log_probs.extend(request_log_probs)
